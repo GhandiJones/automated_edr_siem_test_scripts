1f857b3d31866754d2239f0025537d42a117c37b
ghandijones <DETurner@trustwave.com>
2022-02-18 17:09:37 -0600

    Bad url, updating fetch script again.

--- traffic_scripts/fetch_simple_net_installer.ps1
+++ traffic_scripts/fetch_simple_net_installer.ps1
@@ -1,2 +1,2 @@
-$ScriptLocation = Invoke-WebRequest https://raw.githubusercontent.com/GhandiJones/powershell_scripts/master/traffic_scripts/install_simple_net_installer.ps1
+$ScriptLocation = Invoke-WebRequest https://raw.githubusercontent.com/GhandiJones/powershell_scripts/master/traffic_scripts/install_simple_net_generator.ps1
 Invoke-Expression $($ScriptLocation.Content)
