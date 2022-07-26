90047ee9bc66bff77b620afff01e94bd43ba542c
Daniel E. Turner <DETurner@trustwave.com>
2022-07-26 10:55:47 -0500

    Testing output to Azure.

--- test_pshell_scripts/auto_download_fake_virus.ps1
+++ test_pshell_scripts/auto_download_fake_virus.ps1
@@ -2,3 +2,4 @@ $name = @("death-machine","scary","weird","dragon","monster","wild","shiny","not
 $name = $name + "-virus.txt"
 $path = "$PSScriptRoot\$name"
 Invoke-WebRequest -Uri "https://secure.eicar.org/eicar.com" -OutFile $path
+Write-Output "Completed Eicar testing."
